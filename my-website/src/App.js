36d628f6d113d21f1913523ee31cd08755b28665
Olanbuckley <98430557+Olanbuckley@users.noreply.github.com>
2025-10-23 15:17:26 +0000

    another build

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://storage.googleapis.com/olanbuckleywebsite/Content/Profile%20Photo.jpg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and.
         </p>
